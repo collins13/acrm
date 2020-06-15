4f5a70bfcc3ce48564e604861a332631ff2963e5
rashid collins <rashid@localhost.localdomain>
2020-06-15 16:55:44 +0300

    changes

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'acrm2147'),
             'username' => env('DB_USERNAME', 'african13'),
