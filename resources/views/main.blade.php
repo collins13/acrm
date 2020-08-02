48cc08dd60c86fcdaa3fd2232859a6908bc581be
rashid collins <rashid@localhost.localdomain>
2020-08-02 21:29:24 +0300

    changes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -120,7 +120,7 @@
                 <div class="row">
                     <div class="col-lg-5 col-md-5 col-sm-3 col-xs-3">
                         <div class="slider-content-area">
-                            <div class="slide-text" style="margin-top:60px;">
+                            <div class="slide-text" style="margin-top:70px;">
                                 <h1 class="homepage-three-title">Welcome to <span> AFRICA FOR CHRIST FM RADIO
                                         PROJECTS</span></h1>
                                 {{-- <h2>Africa is largely unentered and the large populaon is unreached by the gospel of Christ. </h2> --}}
